5d94db61b2f8f7f515e901c6ed4c1e4e43b34d7e
MaduraJ <56335187+MaduraJ@users.noreply.github.com>
2022-03-03 17:27:48 +0530

    got headers from the example.net, formated and wrote to a txt

---
+++ IO_CPU Bound/IO_CPU.py
@@ -0,0 +1,7 @@
+import requests
+response= requests.get('https://example.net')
+items=response.headers.items()
+headers=[f'{key}:{header}'for key,header in items]
+formatted_headers = '\n'.join(headers) 
+with open('headers.txt', 'w') as file:
+ file.write(formatted_headers)
